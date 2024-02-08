80b5b82b19a10eff1eb0e1e21325cb23316d32fc
MarcoPeixot <marco.peixoto@sou.inteli.edu.br>
2024-02-08 08:19:04 -0300

    adicionando outros peixes

--- peixe.js
+++ peixe.js
@@ -12,7 +12,7 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-var peixinho;
+var peixinhos;
 
 function preload() {
     //carregando o mar
@@ -22,10 +22,29 @@ function preload() {
     this.load.image('logo', 'assets/logo-inteli_azul.png');
 
     //carregando o peixe
-    this.load.image('peixe', 'assets/peixes/baiacu.png');
+    this.load.image('peixe1', 'assets/peixes/baiacu.png');
+
+    //carregando o peixe
+    this.load.image('peixe2', 'assets/peixes/baiacu_lado.png');
+
+    //carregando o peixe
+    this.load.image('peixe3', 'assets/peixes/peixe_amarelo.png');
+
+    //carregando o peixe
+    this.load.image('peixe4', 'assets/peixes/peixe_listra.png');
+
+    //carregando o peixe
+    this.load.image('peixe5', 'assets/peixes/peixe_logo.png');
+
+    //carregando o peixe
+    this.load.image('peixe6', 'assets/peixes/peixe_serio.png');
+
+    //carregando o peixe
+    this.load.image('peixe7', 'assets/peixes/peixinho_laranja.png');
 
     //carregando o bomba
     this.load.image('bomba', 'assets/peixes/bomba.png');
+
 }
 
 function create() {
@@ -36,13 +55,20 @@ function create() {
 
     //adicionando o bomba no jogo
     this.add.image(400, 300, 'bomba');
+    
+    this.add.image(400, 400, 'peixe2');
+    this.add.image(100, 300, 'peixe3');
+    this.add.image(800, 900, 'peixe4');
+    this.add.image(700, 150, 'peixe5');
+    this.add.image(280, 200, 'peixe6');
+    this.add.image(900, 790, 'peixe7');
 
     //guardar o peixe em uma variável 
-    peixinho = this.add.image(400, 300, 'peixe');
-    peixinho.setFlip(true, false);
+    peixinhos = this.add.image(100, 300, 'peixe1');
+    peixinhos.setFlip(true, false);
 }
 
 function update() {
-    peixinho.x = this.input.x;
-    peixinho.y = this.input.y;
+    peixinhos.x = this.input.x;
+    peixinhos.y = this.input.y;
 }
